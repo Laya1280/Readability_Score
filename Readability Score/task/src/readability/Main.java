5f2dd1e8340fd385dce2d9ba06fee2f179109b8c
Laya1280 <layaml1280@gmail.com>
2020-09-12 16:26:14 +0530

    Initial commit

---
+++ Readability Score/task/src/readability/Main.java
@@ -0,0 +1,13 @@
+package readability;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.nextLine().length() <= 100){
+            System.out.println("EASY");
+        }
+        else{
+            System.out.println("HARD");
+        }
+    }
+}
